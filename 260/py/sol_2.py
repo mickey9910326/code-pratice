5862af530b43e6551b7bfaa6d82a9bde87855fc7
LiYu87 <mickey9910326@gmail.com>
2020-12-17 18:08:41 +0800

    Add sol of 260. Single Number III

---
+++ 260/py/sol_2.py
@@ -0,0 +1,20 @@
+"""260. Single Number III
+   use hash table.
+
+   Time complexity: O(n)
+   Space complexity: O(n)
+"""
+
+import collections
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        c = collections.Counter(nums)
+        ret = []
+
+        for element, count in c.items():
+            if count == 1:
+                ret.append(element)
+
+        return ret
