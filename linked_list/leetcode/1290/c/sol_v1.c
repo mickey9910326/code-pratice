258f5fbf463ecc525f2d8e92e030347d2c637ea2
LiYu87 <mickey9910326@gmail.com>
2021-04-27 17:56:43 +0800

    Add sol of 1290. Convert Binary Number in a Linked List to Integer

---
+++ linked_list/leetcode/1290/c/sol_v1.c
@@ -0,0 +1,24 @@
+/**
+ * @file sol_v1.c
+ * @author LiYu87 (mickey9910326@gmail.com)
+ * @brief leetcode 1290. Convert Binary Number in a Linked List to Integer
+ * @date 2021.04.27
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+int getDecimalValue(struct ListNode* head) {
+    int num = 0;
+    while (head != NULL) {
+        num = num << 1 | head->val;
+        head = head->next;
+    }
+    return num;
+}
