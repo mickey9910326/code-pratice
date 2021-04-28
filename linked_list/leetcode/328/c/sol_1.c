c6e5a21476953b7dcc7a3c386799aa969a2c16af
LiYu87 <mickey9910326@gmail.com>
2021-04-28 21:36:09 +0800

    Add sol of 328. Odd Even Linked List

---
+++ linked_list/leetcode/328/c/sol_1.c
@@ -0,0 +1,43 @@
+/**
+ * @file sol_1.c
+ * @author LiYu87 (mickey9910326@gmail.com)
+ * @brief leetcode 328. Odd Even Linked List
+ * @date 2021.04.28
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct ListNode* oddEvenList(struct ListNode* head) {
+    if (head == NULL) {
+        return head;
+    }
+    if (head->next == NULL) {
+        return head;
+    }
+
+    struct ListNode* odd = head;
+    struct ListNode* even = head->next;
+    struct ListNode* even_head = even;
+
+    while (even && even->next) {
+        odd->next = even->next;
+        odd = odd->next;
+
+        even->next = odd->next;
+        even = even->next;
+    }
+
+    odd->next = even_head;
+
+    return head;
+}
