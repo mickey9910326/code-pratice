7518aeaba747c77ade0189dd59cfb06e85f3a52c
LiYu87 <mickey9910326@gmail.com>
2021-04-30 18:07:04 +0800

    Add sol of 61. Rotate List

---
+++ linked_list/leetcode/61/c/sol_1.c
@@ -0,0 +1,43 @@
+/**
+ * @file sol_1.c
+ * @author LiYu87 (mickey9910326@gmail.com)
+ * @brief leetcode 61. Rotate List
+ * @date 2021.04.30
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ *
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+#include <stdlib.h>
+
+struct ListNode* rotateRight(struct ListNode* head, int k) {
+    if (head == NULL) {
+        return head;
+    }
+
+    struct ListNode* curr = head;
+    int len = 0;
+
+    while (curr->next) {
+        curr = curr->next;
+        len++;
+    }
+    curr->next = head;
+
+    curr = head;
+
+    for (int i = 0; i < len - (k % (len + 1)); i++) {
+        curr = curr->next;
+    }
+
+    head = curr->next;
+    curr->next = NULL;
+
+    return head;
+}
